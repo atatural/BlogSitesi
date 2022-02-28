3b3917449b8f04af0657c0a971b797cd85d3a00b
Ata Tural <ataberktural@galatasaray.net>
2022-02-28 09:10:36 +0300

    28.02.2022

---
+++ WebUI/Blog.WebUI.Site/Controllers/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.WebUI.Site.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index(string slug)
+        {
+            return View();
+        }
+    }
+}
